2f40f36b307c6c61a926f3672b598a1d04860331
Vinicius <vini.gomesm@hotmail.com>
2018-04-22 09:54:35 -0300

    Primeiro.py

---
+++ Primeiro.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+
+i =0
+
+while(i<3):
+    print("O programa iniciou as {}".format(time.ctime()))
+    time.sleep(10)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    i +=1
