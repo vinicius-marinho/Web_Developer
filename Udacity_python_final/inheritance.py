97f84dbf78caeb87bb61528f2767d0ce61d38da4
Vinicius Marinho <vinicius.marinho@corp.globo.com>
2018-04-26 18:26:34 -0300

    Inicio do primeiro projeto

---
+++ Udacity_python_final/inheritance.py
@@ -0,0 +1,24 @@
+class Veiculo():
+    def __init__(self,rodas,combustivel):
+        self.rodas = rodas
+        self.combustivel = combustivel
+
+    def show_info(self):
+        print("Tipo de combustivel: " + self.combustivel)
+
+gol = Veiculo(4,"Gas")
+# print(gol.combustivel)
+gol.show_info()
+
+class Carro(Veiculo): # Classe filha Carro herda da classe pai Veiculo
+    def __init__(self, rodas, combustivel, cor):
+        Veiculo.__init__(self, rodas, combustivel) #Chamo o construtor da classe pai
+        self.cor = cor #variavel apenas da classe filha
+
+    def show_info(self):
+        
+        print("Cor do carro: " + self.cor)
+
+
+porsche = Carro(4, "gasolina", "preta")
+porsche.show_info()
