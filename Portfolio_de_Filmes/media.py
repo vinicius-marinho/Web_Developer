0beb5e143857ecb86bc6a2f71b9db1954128f2a8
Vinicius Marinho <vinicius.marinho@corp.globo.com>
2018-04-26 18:26:10 -0300

    Primeiro portfolio

--- Portfolio_de_Filmes/media.py
+++ Portfolio_de_Filmes/media.py
@@ -1,12 +1,18 @@
 import webbrowser
 
 class Movie():
+
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #Esta variavel esta declarada assim por se tratar de uma constante Ela e uma variavel de classe e nao e inicializada com nenhum objeto
+
+
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
-        self.story = movie_storyline
-        self.image = poster_image
-        self.trailer = trailer_youtube
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
 
     def show_trailer(self):
-        webbrowser.open(self.trailer)
+        webbrowser.open(self.trailer_youtube_url)
 
