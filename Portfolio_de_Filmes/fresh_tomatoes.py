bb00f984468d2e3bb8666041830df2427d5b7f71
Vinicius <vini.gomesm@hotmail.com>
2018-04-29 19:55:21 -0300

    Atualizacao

--- Portfolio_de_Filmes/fresh_tomatoes.py
+++ Portfolio_de_Filmes/fresh_tomatoes.py
@@ -175,3 +175,4 @@ def open_movies_page(movies):
     # open the output file in the browser (in a new tab, if possible)
     url = os.path.abspath(output_file.name)
     webbrowser.open('file://' + url, new=2)
+    
